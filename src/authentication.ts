4c29a88ef4763ea18f0e43dbc5a9e685c7fe6132
Jori Lallo <jtlallo@gmail.com>
2019-10-31 13:39:53 -0700

    Added authentication error

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,16 +1,50 @@
 import { ZObject, Bundle } from "zapier-platform-core";
 
+interface AuthResponse {
+  errors?: {
+    message: string;
+    extensions: {
+      type: string;
+      userError: boolean;
+      userPresentableMessage: string;
+    };
+  }[];
+}
+
+const testAuth = async (z: ZObject, bundle: Bundle) => {
+  const response = await z.request({
+    url: "https://api.linear.app/graphql",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      authorization: bundle.authData.api_key,
+    },
+    body: {
+      query: `
+        query { 
+          user(id: "me") {
+            name
+            email
+          }
+        }
+      `,
+    },
+    method: "POST",
+  });
+
+  const data = response.json as AuthResponse;
+
+  if (data.errors) {
+    throw new Error("Invalid API key.");
+  }
+
+  return response.json;
+};
+
 export const authentication = {
   type: "custom",
 
-  test: {
-    body: { query: 'query { user(id: "me") { name email }}' },
-    url: "https://api.linear.app/graphql",
-    removeMissingValuesFrom: {},
-    headers: { authorization: "{{bundle.authData.api_key}}" },
-    params: {},
-    method: "POST",
-  },
+  test: testAuth,
 
   connectionLabel: (z: ZObject, bundle: Bundle) => {
     const data = bundle.inputData.data;
