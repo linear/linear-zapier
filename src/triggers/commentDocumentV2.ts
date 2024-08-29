e79a68e45533acf4de56da1cb815d595208beae9
Leela Senthil Nathan <leela@linear.app>
2024-08-29 09:18:14 -0700

    Revert "Endpoints"
    This reverts commit 67f4fafcf9193790a9913e655016efd9dd0f7ad6.

--- src/triggers/commentDocumentV2.ts
+++ src/triggers/commentDocumentV2.ts
@@ -67,6 +67,7 @@ const subscribeHook = (z: ZObject, bundle: Bundle) => {
   const data = {
     url: bundle.targetUrl,
     inputData: bundle.inputData,
+    trigger: "commentDocument",
   };
 
   // You may return a promise or a normal data structure from any perform method.
@@ -76,7 +77,7 @@ const subscribeHook = (z: ZObject, bundle: Bundle) => {
 
   return z
     .request({
-      url: "https://linear-dev-zapier.ngrok.io/hooks/zapier/subscribe/commentDocument",
+      url: "https://linear-dev-zapier.ngrok.io/hooks/zapier/subscribe",
       method: "POST",
       body: data,
     })
